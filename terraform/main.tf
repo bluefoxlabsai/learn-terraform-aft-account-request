86cbc689b84703f7da8b39b3747c08f1cd6b1237
Brian Pasquini <brian.pasquini@gmail.com>
2023-04-06 23:40:41 -0400

    Modified account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "testaccount01@bluefoxlabs.ai"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "brian@bluefoxlabs.ai"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
